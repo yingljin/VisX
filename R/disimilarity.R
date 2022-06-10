2c018ca26fb1e6fc31fe71b2a0eebc0b275f8a95
Ying Jin <ying.jin@cuanschutz.edu>
2022-06-10 11:11:13 -0600

    update with new functions

---
+++ R/disimilarity.R
@@ -0,0 +1,138 @@
+
+# this script writes functions for calculating measures of association
+# between different types of variables
+
+#### association ####
+
+#' Calculate association measure between a pair of variables
+#'
+#' @param df a dataframe with 2 columns
+#' @param type a vector with 2 elements corresponding to types of variables in df.
+#' Types can be "numeric", "ordinal" or "factor",
+#' but cannot both be numeric.
+#'
+#' @return values and type of association measure,
+#' as well as p value from corresponding association test
+#'
+#' @details The following associated measures and tests are implemented dependent on variable type:
+#'
+#' factor vs numeric, factor or ordinal: Pseudo R^2 and p value from multinomial regression
+#'
+#' ordinal vs ordinal or numeric: GK gamma and GK gamma correlation test
+#'
+#' @importFrom nnet multinom
+#' @importFrom DescTools PseudoR2
+#' @importFrom car Anova
+#' @importFrom MESS gkgamma
+#'
+#' @examples
+#' data1 <- data.frame(x = rnorm(10), y = rbinom(10, 1, 0.5))
+#' # second variable as factor
+#' type1 <- c("numeric", "factor")
+#' pair_cor(data1, type1)
+#' # second variable as ordinal
+#' type2 <- c("numeric", "ordinal")
+#' pair_cor(data1, type2)
+#'
+pair_cor <- function(df, type){
+
+  # check
+  if(type[1]=="numeric"&type[2]=="numeric"){
+    stop("Not supported")
+  }
+  # pseudo R^2
+  if("factor" %in% type){
+    # multinomial regression
+    varnames <- colnames(df)
+    f <- ifelse(length(unique(df[, 1])) <=  length(unique(df[, 2])),
+                paste(varnames[1], "~", varnames[2]),
+                paste(varnames[2], "~", varnames[1]))
+    f <- as.formula(f)
+    mult_fit <- multinom(f, data = df, model = T)
+    cor_value <- PseudoR2(mult_fit, which = "Nagelkerke")
+    cor_value <- sqrt(cor_value)
+    cor_p <- Anova(mult_fit)$`Pr(>Chisq)`
+    cor_type <- "pseudoR2"
+  }
+  # GK gamma
+  else{
+    #cor_value <- GoodmanKruskalGamma(df[, 1], df[, 2])
+    cor_test <- gkgamma(table(df))
+    cor_value <- cor_test$estimate
+    cor_p <- cor_test$p.value
+    cor_type <- "GKgamma"
+  }
+
+  return(list(cor_value = cor_value, cor_type = cor_type, cor_p = cor_p))
+}
+
+##### pariwise association #####
+
+#' Calculate pariwise association of data with mixed types of variables
+#'
+#' @param df dataframe with mixed types of variables
+#' @param var_type a charater vector corresponding to types of variables in df
+#'
+#' @return matrices with measures, types and p values of association
+#'
+#' @details The following associated measures and tests are implemented dependent on variable type:
+#'
+#' factor vs numeric, factor or ordinal: Pseudo R^2 and p value from multinomial regression
+#'
+#' ordinal vs ordinal or numeric: GK gamma and GK gamma correlation test
+#'
+#' numeric vs numeric: Spearman correlation and p value
+#'
+#' @importFrom Hmisc rcorr
+#' @importFrom nnet multinom
+#' @importFrom DescTools PseudoR2
+#' @importFrom car Anova
+#' @importFrom MESS gkgamma
+#'
+#' @examples
+#' data1 <- data.frame(x = rnorm(10),
+#'  y = rbinom(10, 1, 0.5),
+#' z = rbinom(10, 5, 0.5))
+#' type1 <- c("numeric", "factor", "ordinal")
+#' pairwise_cor(data1, type1)
+#'
+pairwise_cor <- function(df, var_type){
+
+  # set-up
+  p <- ncol(df)
+  id_num <- which(var_type == "numeric")
+  id_non_num <- which(var_type != "numeric")
+
+  # container
+  cor_value_mat <- matrix(NA, p, p)
+  cor_type_mat <- matrix(NA, p, p)
+  cor_p_mat <- matrix(NA, p, p)
+
+  colnames(cor_value_mat) <- rownames(cor_value_mat) <- colnames(df)
+  colnames(cor_type_mat) <- rownames(cor_type_mat) <- colnames(df)
+  colnames(cor_p_mat) <- rownames(cor_p_mat) <- colnames(df)
+
+  # calculate correlation
+  ## numeric variables alone
+  if(!is.null(id_num)){
+    num_cor <-  rcorr(as.matrix(df[, id_num]), type = "spearman")
+    cor_value_mat[id_num, id_num] <- num_cor$r
+    cor_p_mat[id_num, id_num] <- num_cor$P
+    cor_type_mat[id_num, id_num] <- "spearman"
+  }
+
+  ## other types
+  if(!is.null(id_non_num)){
+    for(i in  1:p){
+      for(j in id_non_num){
+        this_cor <- pair_cor(df[, c(i, j)], var_type[c(i, j)])
+        cor_value_mat[i, j] <- cor_value_mat[j, i] <- this_cor$cor_value
+        cor_type_mat[i, j] <- cor_type_mat[j, i] <- this_cor$cor_type
+        cor_p_mat[i, j] <- cor_p_mat[j, i] <- this_cor$cor_p
+      }
+    }
+  }
+
+
+  return(list(cor_value = cor_value_mat, cor_type = cor_type_mat, cor_p = cor_p_mat))
+}
---
+++ R/npc_mixed_cor.R
@@ -0,0 +1,196 @@
+
+#' Network Plot of Association
+#'
+#' @param cor_value a matrix with values of association measures
+#' @param cor_type a matrix with types of association measures
+#' @param cor_p a matrix with p values of association tests
+#' @param var_type a character vector with types of variables association is measured on
+#' @param min_cor minimum association to be visualized
+#' @param show_signif whether or not to visualize significance of association
+#' @param sig.level maximum significance to be visualized
+#' @param overlay if TRUE, only show significant association above min_cor threshold
+#' @param legend if TURE, how legend
+#' @param repel if TRUE, text labels of variable names repel each other
+#' @param label_size size of text labels of variable names
+#'
+#' @return A network plot of association
+#'
+#' @importFrom Hmisc rcorr
+#' @importFrom nnet multinom
+#' @importFrom DescTools PseudoR2
+#' @importFrom car Anova
+#' @importFrom MESS gkgamma
+#' @import ggplot2
+#' @import ggnewscale
+#' @import ggrepel
+#'
+#' @examples
+#' data("mtcars")
+#' type1 <- c(rep("numeric", 7), rep("factor", 2), rep("ordinal", 2))
+#' test_cor1 <- pairwise_cor(mtcars, type1)
+#' npc_mixed_cor(test_cor1$cor_value, test_cor1$cor_type, test_cor1$cor_p, type1)
+#'
+#'
+npc_mixed_cor <- function (cor_value, cor_type, cor_p, var_type,
+                           sig.level = 0.05, min_cor = 0.3,
+                           show_signif = FALSE,
+                           legend = TRUE, repel = TRUE, label_size = 5,
+                           overlay = TRUE){
+  ## A few checks
+  if(min_cor < 0 || min_cor > 1){
+    stop("min_cor must be a value ranging from zero to one.")
+  }
+  if(!isSymmetric(cor_value) | !isSymmetric(cor_type)){
+    stop("Correlation matrix or type not valid")
+  }
+
+  # create dissimilarity matrix
+  dissim <- 1-abs(cor_value)
+
+  # Calculate coordinates of variables
+  points <-
+    if (ncol(dissim) == 1) {
+      matrix(c(0, 0), ncol = 2, dimnames = list(colnames(dissim)))
+    }  else if (ncol(dissim) == 2) {
+      matrix(c(0, -0.1, 0, 0.1), ncol = 2, dimnames = list(colnames(dissim)))
+    }  else {
+      suppressWarnings(cmdscale(dissim, k = 2))
+    }
+  if (ncol(points) < 2) {
+    cont_flag <- FALSE
+    shift_matrix <- matrix(1, nrow = nrow(dissim), ncol = ncol(dissim))
+    diag(shift_matrix) <- 0
+    for (shift in 10^(-6:-1)) {
+      shifted_distance <- dissim + shift * shift_matrix
+      points <- suppressWarnings(stats::cmdscale(shifted_distance))
+      if (ncol(points) > 1) {
+        cont_flag <- TRUE
+        break
+      }
+    }
+    if (!cont_flag)
+      rlang::abort("Can't generate network plot.\nAttempts to generate 2-d coordinates failed.")
+    rlang::warn("Plot coordinates derived from correlation matrix have dimension < 2.\nPairwise distances have been adjusted to facilitate plotting.")
+  }
+  points <- data.frame(points)
+  colnames(points) <- c("x", "y")
+  points$id <- rownames(points)
+
+  # edges according to value of correlation
+  proximity <- abs(cor_value)
+  proximity[upper.tri(proximity)] <- NA
+  diag(proximity) <- NA
+  proximity[proximity < min_cor] <- NA
+  n_paths <- sum(!is.na(proximity))
+  paths <- data.frame(matrix(nrow = n_paths, ncol = 7))
+  colnames(paths) <- c("x", "y", "xend", "yend", "proximity", "sign", "type")
+  path <- 1
+  for (row in 1:nrow(proximity)) {
+    for (col in 1:ncol(proximity)) {
+      path_proximity <- proximity[row, col]
+      if (!is.na(path_proximity)) {
+        path_sign <- sign(cor_value[row, col])
+        x <- points$x[row]
+        y <- points$y[row]
+        xend <- points$x[col]
+        yend <- points$y[col]
+        paths[path, ] <- c(x, y, xend, yend, path_proximity,
+                           path_sign, cor_type[row, col])
+        path <- path + 1
+      }
+    }
+  }
+  paths <- paths %>% mutate_at(vars(-type), as.numeric)
+
+  # edges based on significance
+  if(show_signif){
+    proximity2 <- cor_p
+    proximity2[upper.tri(proximity2)] <- NA
+    diag(proximity2) <- NA
+    proximity2[proximity2 > sig.level] <- NA
+    ### whether or not show all significance
+    if(overlay){
+    proximity2[is.na(proximity)] <- NA
+    }
+    n_paths2 <- sum(!is.na(proximity2))
+    paths2 <- data.frame(matrix(nrow = n_paths2, ncol = 5))
+    colnames(paths2) <- c("x", "y", "xend", "yend", "proximity")
+    path <- 1
+    for (row in 1:nrow(proximity2)) {
+      for (col in 1:ncol(proximity2)) {
+        path_proximity <- proximity2[row, col]
+        if (!is.na(path_proximity)) {
+          x <- points$x[row]
+          y <- points$y[row]
+          xend <- points$x[col]
+          yend <- points$y[col]
+          paths2[path, ] <- c(x, y, xend, yend, path_proximity)
+          path <- path + 1
+        }
+      }
+    }
+  }
+
+  # plot correlation
+  ## color by correlation type, point shape by variable type
+  color_var <- ifelse(paths$type=="pseudoR2", "nodir", "dir")
+  paths <- split(paths, f = color_var)
+  ## add var_type in points
+  #points$var_type <- as.numeric(var_type)
+  ## plot using different colorscales
+  npc <- ggplot()+
+    geom_curve(data = paths$dir,
+               aes(x = x, y = y, xend = xend, yend = yend,
+                   alpha = proximity, size = proximity,
+                   color = proximity*sign))+
+    scale_alpha(limits = c(0, 1))+
+    scale_size(limits = c(0, 1))+
+    scale_color_gradientn(name = "Spearman/GKgamma", limits = c(-1, 1),
+                          colors = c("indianred2","white", "skyblue1"))+
+    new_scale_color()+
+    geom_curve(data = paths$nodir,
+               aes(x = x, y = y, xend = xend, yend = yend,
+                   alpha = proximity, size = proximity,
+                   color = proximity))+
+    scale_color_gradientn(name = "PseudoR2", limits = c(0, 1),
+                          colors = c("white", "darkorange"))+
+    geom_point(data = points, aes(x,y, shape = var_type), size = 3,
+               alpha=1, colour = "black")+
+    scale_shape_manual(name = "Variable type", values = c(1, 2, 5))
+  # add significance level
+  if(show_signif){
+    npc <- npc+
+      geom_curve(data = paths2,
+                aes(x = x, y = y, xend = xend, yend = yend, alpha = 0.5))
+  }
+
+  # add variable names and legend
+  npc_labs <- list(# variable label
+              if(repel)
+                ggrepel::geom_text_repel(data = points,aes(x, y, label = id),
+                                         fontface = "bold",size = label_size,
+                                         segment.size = 0,
+                                         segment.color = "white"),
+              if(!repel)
+                geom_text(data = points,aes(x, y, label = id),
+                          fontface = "bold",
+                          size = label_size),
+              expand_limits(x = c(min(points$x) - 0.1, max(points$x) +0.1),
+                            y = c(min(points$y) - 0.1, max(points$y) +0.1)),
+              theme_void(),
+              guides(size = "none", alpha = "none"),
+              # legend
+              if (legend)
+                theme(legend.text = element_text(size = 10),
+                      legend.title = element_text(face = "bold"),
+                      legend.key.size = unit(0.7, "cm"),
+                      legend.spacing.y = unit(0.5, "cm")),
+              if (!legend)
+                theme(legend.position = "none"))
+  # fianl plot
+  npc+npc_labs
+}
+
+
+
+
